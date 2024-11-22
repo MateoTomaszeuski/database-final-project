8d32e06bf40b7e38de6aeb8fe45f07fa134a51e4
root <root@ENGRJJCP5S3.ad.snow.edu>
2024-11-22 11:50:43 -0700

    DML is not needed

--- DML/feature_pack.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO gamestore.feature_pack (feature_name,cost_auto_renewal_month,cost_auto_renewal_year,cost_not_auto_renewal_month,cost_not_auto_renewal_year) VALUES
-	 ('Retro Pack',$2.99,$29.99,$3.99,$39.99),
-	 ('Multiplayer Pack',$2.50,$24.99,$3.49,$34.99),
-	 ('Dungeon Pack',$2.99,$29.99,$3.99,$39.99),
-	 ('Crossover Pack',$4.99,$49.99,$5.99,$59.99);
--- DML/subscription.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO gamestore."subscription" (subc_name,cost_month,cost_year,concurrent_logins) VALUES
-	 ('Gold',$9.99,$99.99,5),
-	 ('Silver',$7.99,$69.99,2),
-	 ('Bronze',$4.99,$49.99,1);
